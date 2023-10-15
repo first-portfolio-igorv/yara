c77675d6301286f361e27e1ff8f34d16064160d7
first-portfolio-igorv <102622018+first-portfolio-igorv@users.noreply.github.com>
2023-10-15 21:41:08 +0300

    Add files via upload

--- editing.js
+++ editing.js
@@ -2,6 +2,7 @@ let block = document.getElementsByClassName("block");
 let title = document.getElementsByClassName("clickTitle");
 let text = document.getElementsByClassName("text");
 let height = [14.72,11.66,14.51,15.76,16.11,13.95,15.69,12.84,19.09,15.97,12.56,11.11,10.41,11.66,10.41];
+let maxHeight=[];
 let check = [];
 let en=document.getElementsByClassName("en-button");
 let ua=document.getElementsByClassName("ukr-button");
@@ -10,6 +11,9 @@ let EN=document.getElementsByClassName("EN");
 let line=document.getElementsByClassName("comments-line");
 let circle=document.getElementsByClassName("progress-line");
 let position=0;
+for(let i=0;i<height.length;i++){
+  maxHeight.push(height[i]+8)
+}
 en[0].addEventListener("click", function(){
     for(let i=0;i<UA.length;i++){
         UA[i].style.display="none";
@@ -27,19 +31,30 @@ function open(i){
     check[i] = false;
     for (let i = 0; i < block.length; i++) {
       if (block[i].id == event.target.id) {
+        if(window.innerWidth<500){
+          block[i].style.height = maxHeight[event.target.id] + "vw";
+        }
+        else{
         block[i].style.height = height[event.target.id] + "vw";
+
+        }
       }
     }
     for (let i = 0; i < text.length; i++) {
       if (text[i].id == event.target.id) {
-        text[i].style.maxHeight = "300px";
+        text[i].style.maxHeight = "500px";
       }
     }
   } else {
     check[i] = true;
     for (let i = 0; i < block.length; i++) {
       if (block[i].id == event.target.id) {
-        block[i].style.height = "7.29vw";
+        if(window.innerWidth<500){
+        block[i].style.height = "10vw";
+        }
+        else{
+          block[i].style.height = "7.29vw";
+        }
       }
     }
     for (let i = 0; i < text.length; i++) {
@@ -55,24 +70,24 @@ for (let i = 0; i < block.length; i++) {
     open(i);
   });
 }
-// function right(){
-//   if(position==-150){
-//     position=0;
-//   }
-//   else{
-//     position-=50;
-//   }
+function right(){
+  if(position==-150){
+    position=0;
+  }
+  else{
+    position-=50;
+  }
   
-//   line[0].style.left=position+"vw";
+  line[0].style.left=position+"vw";
 
-// }
-// function left(){
-//   if(position==0){
-//     position=0;
-//   }
-//   else{
-//     position+=50;
-//   }
+}
+function left(){
+  if(position==0){
+    position=0;
+  }
+  else{
+    position+=50;
+  }
 
-//   line[0].style.left=position+"vw";
-// }
+  line[0].style.left=position+"vw";
+}
